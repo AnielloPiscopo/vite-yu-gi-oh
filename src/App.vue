3d21d6f3415b218f4c4e8467df4f7c22b56b8ea8
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-11 15:32:27 +0100

    Created the first components, style file and added the reset to the project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import AppHeader from './components/header/AppHeader.vue';
+import AppMain from './components/main/AppMain.vue';
+
+
+export default {
+    conponents: {
+        AppHeader,
+        AppMain,
+    }
+}
 </script>
 
+
 <template>
+    <AppHeader />
+    <AppMain />
 </template>
 
-<style scoped>
 
+<style lang="scss">
+@use './styles/general.scss' as *;
 </style>
